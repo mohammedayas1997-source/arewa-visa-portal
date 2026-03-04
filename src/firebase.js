30825c74f935990df7ccc10c71f289766a023a1d
Abdulrahman <mohammedayas1997@gmail.com>
2026-03-03 22:57:45 -0800

    Fix: Exported db and auth to resolve login system error

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getDatabase } from "firebase/database";
-import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB1miiQVdgce8GFDhvD5v03kf9Pbz_rqlY",
   authDomain: "arewa-visa-portal.firebaseapp.com",
-  projectId: "arewa-visa-portal",
   databaseURL: "https://arewa-visa-portal-default-rtdb.firebaseio.com",
+  projectId: "arewa-visa-portal",
   storageBucket: "arewa-visa-portal.firebasestorage.app",
   messagingSenderId: "230984227020",
   appId: "1:230984227020:web:4778678a0f6252fae98a69",
@@ -16,8 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-// MUN BAMBANTA SUNAYEN DOMIN KOWANE SHAFIN YA SAMU ABINDA YAKE BUQATA
-export const firestore = getFirestore(app); // StaffLogin zai yi amfani da wannan
-export const db = getDatabase(app); // Sauran shafukan za su ci gaba da amfani da 'db'
-export const storage = getStorage(app);
+// WADANNAN SUNE MASU MUHIMMANCI GA LOGIN
+export const db = getFirestore(app);
 export const auth = getAuth(app);
