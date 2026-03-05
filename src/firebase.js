58537df7c385e400d4204ae15fbfa6d3c60a117b
Abdulrahman <mohammedayas1997@gmail.com>
2026-03-04 22:00:48 -0800

    Switch: Migrated to new Firebase project arewa-visa-new1 to resolve billing issues

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,24 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore"; 
+import { getAuth } from "firebase/auth";           
 import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1miiQVdgce8GFDhvD5v03kf9Pbz_rqlY",
-  authDomain: "arewa-visa-portal.firebaseapp.com",
-  projectId: "arewa-visa-portal",
-  databaseURL: "https://arewa-visa-portal-default-rtdb.firebaseio.com",
-  storageBucket: "arewa-visa-portal.firebasestorage.app",
-  messagingSenderId: "230984227020",
-  appId: "1:230984227020:web:4778678a0f6252fae98a69",
+  apiKey: "AIzaSyBouYyVd9xanc9BNvgMhUBj745ufh_h6FQ",
+  authDomain: "arewa-visa-new1.firebaseapp.com",
+  projectId: "arewa-visa-new1",
+  storageBucket: "arewa-visa-new1.firebasestorage.app",
+  messagingSenderId: "609831868003",
+  appId: "1:609831868003:web:90eaecaa973a147e7d8a56",
+  measurementId: "G-MD8K7541V3"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// MUN BAMBANTA SUNAYEN DOMIN KOWANE SHAFIN YA SAMU ABINDA YAKE BUQATA
-export const firestore = getFirestore(app); // StaffLogin zai yi amfani da wannan
-export const db = getDatabase(app); // Sauran shafukan za su ci gaba da amfani da 'db'
-export const storage = getStorage(app);
+// Muna fitar da su (Export) domin login ya yi aiki
+export const db = getFirestore(app); 
 export const auth = getAuth(app);
+export const storage = getStorage(app);
+
+export default app;
