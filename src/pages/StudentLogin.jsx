1ab3f2714df770c9484d3c4150b86d71349281c5
Abdulrahman <mohammedayas1997@gmail.com>
2026-03-07 13:47:23 -0800

    Fix: Aligned Firebase imports using aliases to prevent blank screen. Staff and Student login now correctly reference firestore instance.

--- src/pages/StudentLogin.jsx
+++ src/pages/StudentLogin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { auth, db } from "../firebase";
+import { auth, firestore as db } from "../firebase";
 import {
   signInWithEmailAndPassword,
   signOut,
